151143da6457e654357b0bc6d43a551fad6daf78
4dragunov <4dragunov@gmail.com>
2021-02-15 18:23:46 +0300

    final

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -129,11 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'redoc/')
+
 
 
 # AUTH_USER_MODEL = 'api.User'
